9e65b61fcbe1dc2a009fcc266c3d1b32705e4180
Mr-Sean <86028295+Mr-Sean@users.noreply.github.com>
2021-10-17 02:22:36 -0700

    redo

---
+++ JavaScript/J1_core1/prac.js
@@ -0,0 +1,9 @@
+const [text, setText] = useState("");
+setText("state state I am setting state!!!");
+console.log(text);
+
+// const [arr, setArr] = useState([]);
+// for(let i=1; i<=5; i++) {
+//     setArr(arr.push(i));
+// }
+// console.log(arr);
---
+++ React/R3_core4/put_it_together/src/components/PersonCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+const PersonCard = (props) => {
+
+    const {firstName, lastName, age, hairColor} = props;
+
+
+
+
+    return (
+        <div className="personcard">
+            <h2>{lastName}, {firstName}</h2>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
+
+        </div>
+    );
+
+}
+
+export default PersonCard;
